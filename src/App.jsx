9cd4aedea317fd9623ece899805fe404395d32de
Abhimanyuap <97864658+Abhimanyuap@users.noreply.github.com>
2024-06-03 11:38:21 +0530

    player and sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Player from './components/Player'
+import Sidebar from './components/Sidebar'
 
 function App() {
 
   return (
-    <div className='App'>
-     
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+        <Sidebar />
+      </div>
+      <Player />
     </div>
   )
 }
